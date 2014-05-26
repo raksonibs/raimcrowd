7caa43d7dcaf0086e05fef2b21dd6d6a7b9d781d
Hammad Siddiqui <hammadzs@gmail.com>
2014-05-25 22:18:34 -0400

    s3 configuration

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,13 @@
 CarrierWave.configure do |config|
-  if Rails.env.production? and Configuration[:aws_access_key]
+  if Rails.env.production? and ENV['AWS_ACCESS_KEY']
     config.fog_credentials = {
       provider: 'AWS',
       host: 's3.amazonaws.com',
       endpoint: 'https://s3.amazonaws.com',
-      aws_access_key_id: Configuration[:aws_access_key],
-      aws_secret_access_key: Configuration[:aws_secret_key]
+      aws_access_key_id: ENV['AWS_ACCESS_KEY'],
+      aws_secret_access_key: ENV['AWS_SECRET_KEY']
     }
-    config.fog_directory  = Configuration[:aws_bucket]
+    config.fog_directory  = ENV['AWS_BUCKET_NAME']
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
   else
     config.enable_processing = false if Rails.env.test? or Rails.env.cucumber?
