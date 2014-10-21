684d4a7529154f1585b3f10ea0e14ce75c93b113
Hammad Siddiqui <hammadzs@gmail.com>
2014-10-20 22:13:18 -0400

    Disabled Balanced Payments engine

--- vendor/gems/neighborly-balanced-bankaccount-1.0.0/config/initializers/registration.rb
+++ vendor/gems/neighborly-balanced-bankaccount-1.0.0/config/initializers/registration.rb
@@ -1,5 +1,6 @@
 begin
-  PaymentEngine.new(Neighborly::Balanced::Bankaccount::Interface.new).save
+  # Uncomment the following line to enable Balanced Bank Account engine again.
+  # PaymentEngine.new(Neighborly::Balanced::Bankaccount::Interface.new).save
 rescue Exception => e
   puts "Error while registering payment engine: #{e}"
 end
