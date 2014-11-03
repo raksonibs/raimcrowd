5c0e1b2dbc52e2a858ff2f85f046ec3a902cacbd
Oskar Niburski <oskarniburski@gmail.com>
2014-11-03 11:15:18 -0500

    Remove blur for background images

--- app/helpers/hero_header_tag_helper.rb
+++ app/helpers/hero_header_tag_helper.rb
@@ -2,6 +2,6 @@ module HeroHeaderTagHelper
   def hero_header_tag(object, options = {class: nil}, &block)
     content_tag :header, capture(&block),
       class: [:hero, options[:class], "#{'no-image' unless object.hero_image_url.present?}"],
-      data: { :"image-url" => image_url(object.hero_image_url(:blur)) }
+      data: { :"image-url" => image_url(object.hero_image_url) }
   end
 end
