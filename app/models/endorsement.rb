689bdee5745c11fec6a4c9e1be6ade55605e282a
Oskar Niburski <oskarniburski@gmail.com>
2014-11-06 12:22:23 -0500

    Validation of prescence and catchall

--- app/models/endorsement.rb
+++ app/models/endorsement.rb
@@ -3,6 +3,7 @@ class Endorsement < ActiveRecord::Base
   belongs_to :project
 
   validates_uniqueness_of :user_id, :scope => [:project_id]
+  validates_presence_of :user_id
 
   scope :by_project, ->(project) { where(project_id: project) }
 end
